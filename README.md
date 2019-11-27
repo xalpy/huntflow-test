ca20a643f961aee288491853b09c0a3abab690df
xalpy <drozdisoser@gmail.com>
2019-11-27 15:15:24 +0300

    v0.1(not final)

---
+++ README.md
@@ -0,0 +1,20 @@
+# Тестовое задание для huntflow
+
+## Для запуска:
+
+### 1) Перейдите в папку с файлом
+
+### 2) Запуск скрипта
+
+```python
+python3 huntflow.py argv1 argv2 
+```
+Где argv1 - путь к папке с ексель файлом и папками вакансий
+
+Где argv2 - access_token 
+
+## Requerements
+```python
+requests
+xlrd
+```
---
+++ huntflow.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Nikita Markozov"
+
+import sys
+import os
+import re
+
+import xlrd
+import requests
+from requests.models import Response
+
+
+def upload_file(name: str, path: str, headers: str) -> Response:
+	url = 'https://dev-100-api.huntflow.ru/account/2/upload'
+	files = {'file': (f'{name}.pdf', open(f'{path}', 'rb'), 'application/pdf')}
+	req_resume = requests.post(url, headers=headers, files=files).json()
+	print(type(req_resume))
+	return req_resume
+
+
+def create_body_to_req(last_name, first_name, middle_name, position, money,
+						birth_day, month, year, photo, text, resume_id) -> dict:
+	body = {
+		"last_name": last_name,
+		"first_name": first_name,
+		"middle_name": middle_name,
+		"position": position,
+		"money": money,
+		"birthday_day": birth_day,
+    	"birthday_month": month,
+    	"birthday_year": year,
+    	"photo": photo,
+		"externals": [
+		{
+			"data":{
+				"body": f"{text}",
+			},
+			"auth_type": "NATIVE",
+			"files": [{
+				"id": resume_id
+			}]
+		}
+		]
+	}
+	return body
+
+
+def request_to_add_applicant(info_list: list, resume: dict, headers: dict) -> int:
+	name_info = str(info_list[1].value)
+	last_name = name_info.split()[0]
+	first_name = name_info.split()[1]
+	position = info_list[0].value
+	money = str(info_list[2].value)
+	try:
+		middle_name = name_info.split()[2]
+	except IndexError as e:
+		middle_name = None
+	photo = resume['photo']['id']
+	text = resume['text']
+	resume_id = resume['id']
+	try:
+		birth = resume['fields']['birthdate']
+		birth_day = birth['day']
+		month = birth['month']
+		year = birth['year']
+
+	except (KeyError, TypeError) as e:
+		birth_day = None
+		month = None
+		year = None
+
+	url = 'https://dev-100-api.huntflow.ru/account/2/applicants'
+
+	body = create_body_to_req(last_name, first_name, middle_name, position, money,
+						birth_day, month, year, photo, text, resume_id)
+
+	json_adding_applicant = requests.post(url, json=body, headers=headers).json()
+	return json_adding_applicant['id']
+	
+
+
+def mark_request(list_: list, headers: dict) -> Response:
+	for i in list_:
+		body = {
+			"color": "d7b100",
+			"name": f"{i[-1]}"
+		}
+		req = requests.post('https://dev-100-api.huntflow.ru/account/2/applicants', 
+							json=body, headers=headers) 
+		
+		return req
+		
+	
+
+def find_resume_files(path: str, folder: str, name: str) -> str:
+	
+	local_path = path + '/' + str(folder.value)
+	for i in os.listdir(local_path):
+		splited_name_value = str(name.value).split()[0]
+		if re.search(splited_name_value, i):
+			file_path = local_path + '/' + i
+			return file_path
+
+	
+
+
+
+def xlsx(path: str) -> list:
+	files = os.listdir(path) # self.path
+	xls = [i for i in files if re.search('.xlsx', i)]
+	xl_book_path = re.sub(' ', '', path + '/' + xls[0])
+	book = xlrd.open_workbook(xl_book_path)
+	sh = book.sheet_by_index(0)
+	row_with_info = [sh.row(rx) for rx in range(1, sh.nrows)]
+	return row_with_info
+
+
+
+
+
+def main(path: str, token: str) -> str:
+	headers = {
+		'User-Agent': 'App/1.0 (drozdivoron@gmail.com)',
+    	'Authorization': f'Bearer {token}',
+	    'X-File-Parse': 'true',
+	}
+
+	infos = xlsx(path)
+	list_with_resume = [find_resume_files(path, i[0], i[1]) for i in infos]
+	for_itter = range(len(infos))
+	resumes_json = [upload_file(infos[i][1], list_with_resume[i], headers) for i in for_itter]
+	applicants_id = [request_to_add_applicant(infos[i], resumes_json[i], headers) for i in for_itter]
+	
+
+	mark_request(infos, headers)
+	return 'Done'
+
+
+
+if __name__ == '__main__':
+	refactor_path = sys.argv[1].replace('\\', '/')
+	if os.listdir(refactor_path):
+		try:
+			main(rf'{sys.argv[1]}', sys.argv[2])
+		except TypeError as e:
+			print(e)
+	else:
+		print('Папки не существует')
